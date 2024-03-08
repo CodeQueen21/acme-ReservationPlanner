0c83e7542ed698ad65d06c1cbfc551b0eefed818
Raelinda Shoates <redfire809@gmail.com>
2024-03-08 11:41:09 -0500

    created tables and added customers/restuarants

--- server/db.js
+++ server/db.js
@@ -1,9 +1,75 @@
 const pg = require("pg");
+const uuid = require("uuid");
 
 const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/acme_reservations_db"
 );
 
+const createTables = async () => {
+  const SQL = `
+DROP TABLE IF EXISTS reservations;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS restaurants;
+
+CREATE TABLE customers(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE restaurants(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE reservations(
+    id UUID PRIMARY KEY,
+    customer_id UUID REFERENCES customers(id) NOT NULL,
+    restaurant_id UUID REFERENCES restaurants(id) NOT NULL,
+    date DATE NOT NULL,
+    party_count INTEGER NOT NULL
+    
+);
+`;
+  await client.query(SQL);
+};
+
+const createCustomer = async (name) => {
+  const SQL = `
+    INSERT INTO customers(id, name) Values($1, $2) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+
+const createRestaurant = async (name) => {
+  const SQL = `
+    INSERT INTO restaurants(id, name) VALUES($1, $2) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+
+const fetchCustomers = async () => {
+  const SQL = `
+    SELECT * FROM customers
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchRestaurants = async () => {
+  const SQL = `
+      SELECT * FROM restaurants
+      `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
 module.exports = {
   client,
+  createTables,
+  createCustomer,
+  createRestaurant,
+  fetchCustomers,
+  fetchRestaurants,
 };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,45 @@
-const { client, createTables } = require("./db");
+const express = require("express");
+const app = express();
+app.use(require("morgan")("dev"));
+app.use(express.json());
+const port = 3000;
+
+const {
+  client,
+  createTables,
+  createCustomer,
+  createRestaurant,
+  fetchCustomers,
+  fetchRestaurants,
+} = require("./db");
 
 const init = async () => {
   await client.connect();
   console.log("connected to database");
+  await createTables();
+  console.log("tables created");
+  const [
+    Lisa,
+    Brad,
+    James,
+    Carol,
+    Olive_Garden,
+    Pizza_Hut,
+    Chipotle,
+    SteakHouse,
+  ] = await Promise.all([
+    createCustomer("Lisa"),
+    createCustomer("Brad"),
+    createCustomer("James"),
+    createCustomer("Carol"),
+    createRestaurant("Olive_Garden"),
+    createRestaurant("Pizza_Hut"),
+    createRestaurant("Chipotle"),
+    createRestaurant("SteakHouse"),
+  ]);
+  console.log(await fetchCustomers());
+  console.log(await fetchRestaurants());
+  app.listen(port, () => console.log(`listening on port ${port}`));
 };
 
 init();
